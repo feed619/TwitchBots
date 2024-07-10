82c66fc0e70106fe492db7cf3211a02491e55125
feed228 <azimovpro@gmail.com>
2024-07-10 12:48:40 +0300

    update

--- src/app/client.py
+++ src/app/client.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://127.0.0.1:5000/sent_data'
-data = {'data': 'Golovach top'}
+data = {'data': 'Golovach top data Golovach top '}
 
 response = requests.post(url, json=data)
 print(response.json())

9d2ba3b361b5870d033dd96e8acef68dccf55ed6
feed228 <azimovpro@gmail.com>
2024-07-10 13:10:15 +0300

    update application

--- widget/main.cpp
+++ widget/main.cpp
@@ -1,11 +1,17 @@
-#include "mainwindow.h"
 
 #include <QApplication>
+#include <QWidget>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    window.resize(250, 150);
+    window.setWindowTitle("Simple example");
+    window.show();
+
+    return app.exec();
 }
